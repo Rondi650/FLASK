9d03083a18e059754ebe3b05aae185c8cd23eb63
Rondi650 <rondinellenunes123@gmail.com>
2025-08-21 15:48:09 -0300

    validacao session e redirect

--- app.py
+++ app.py
@@ -17,10 +17,14 @@ lista = [jogo1,jogo2,jogo3]
 
 @app.route('/')
 def index():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('login')    
     return render_template('lista.html', jogos = lista, titulo = 'Jogos')
 
 @app.route('/novo', methods = ['GET'])
 def novo_jogo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('login')
     return render_template('novo.html', titulo = 'Inserir Novo Jogo')
 
 @app.route('/criar', methods = ['POST'])
@@ -41,7 +45,7 @@ def autenticar():
     if '123' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado'] + ' logado com sucesso!')
-        return redirect('/')
+        return redirect('/novo')
     else:
         flash('Usuario ou senha incorreto.')
         return redirect('/login')
@@ -50,6 +54,6 @@ def autenticar():
 def logout():
      session['usuario_logado'] = None
      flash('Logout efetuado com sucesso!')
-     return redirect('/') 
+     return redirect('/login') 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
