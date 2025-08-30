e5e36737315ce07af999dd1eb70414f9179e9efd
Rondi650 <rondinellenunes123@gmail.com>
2025-08-30 18:35:25 -0300

    organizacao codigo ORM separado arquivos

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
+from conexao_bd import db, Usuario, Jogo, init_app
 
 app = Flask(__name__)
 app.secret_key = 'Rondi'
 
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-        
-usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
-usuario2 = Usuario("Camila Ferreira", "Mila", "paozinho")
-usuario3 = Usuario("Guilherme Louro", "Cake", "python_eh_vida")
-usuario4 = Usuario('Rondinelle Oliveira','Rondi', '123')
-
-usuarios = { usuario1.nickname :usuario1, 
-                usuario2.nickname :usuario2,
-                usuario3.nickname :usuario3,
-                usuario4.nickname: usuario4}
-
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
- 
-jogo1 = Jogo("The Legend of Zelda: Breath of the Wild", "Aventura", "Nintendo Switch")
-jogo2 = Jogo("God of War Ragnarök", "Ação", "PlayStation 5")
-jogo3 = Jogo("Halo Infinite", "FPS", "Xbox Series X")  
-
-lista = [jogo1,jogo2,jogo3]
+# Inicializa o db com o app
+init_app(app)
 
 @app.route('/login')
 def login():
@@ -39,21 +15,30 @@ def login():
 def index():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login'))   
-    return render_template('lista.html', jogos = lista, titulo = 'Jogos')
+    
+    # Buscar jogos - agora com Flask-SQLAlchemy
+    jogos = Jogo.query.all()
+    
+    return render_template('lista.html', jogos=jogos, titulo='Jogos')
 
-@app.route('/novo', methods = ['GET'])
+@app.route('/novo', methods=['GET'])
 def novo_jogo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login'))
-    return render_template('novo.html', titulo = 'Inserir Novo Jogo')
+    return render_template('novo.html', titulo='Inserir Novo Jogo')
 
-@app.route('/criar', methods = ['POST'])
+@app.route('/criar', methods=['POST'])
 def criar_jogo():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-    novo_jogo = Jogo(nome,categoria,console)
-    lista.append(novo_jogo)
+    
+    # Salvar com Flask-SQLAlchemy
+    jogo = Jogo(nome=nome, categoria=categoria, console=console)
+    db.session.add(jogo)
+    db.session.commit()
+    
+    flash('Jogo criado com sucesso!')
     return redirect(url_for('index'))
 
 @app.route('/autenticar', methods=['POST'])
@@ -61,20 +46,22 @@ def autenticar():
     nickname = request.form['usuario']
     senha = request.form['senha']
     
-    if nickname in usuarios:
-        usuario = usuarios[nickname]
-        if senha == usuario.senha:
-            session['usuario_logado'] = usuario.nickname
-            flash(usuario.nickname + ' logado com sucesso')
+    # Buscar com Flask-SQLAlchemy
+    usuario = Usuario.query.filter_by(nickname=nickname).first()
+    
+    if usuario and senha == usuario.senha:
+        session['usuario_logado'] = usuario.nickname
+        flash(usuario.nickname + ' logado com sucesso')
         return redirect(url_for('novo_jogo'))
     else:
         flash('Usuario ou senha incorreto.')
         return redirect(url_for('login'))
-    
+
 @app.route('/logout')
 def logout():
-     session['usuario_logado'] = None
-     flash('Logout efetuado com sucesso!')
-     return redirect(url_for('login'))
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('login'))
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
